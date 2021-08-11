f78b0b15a7758d231fbf6cbac89a45bdfcdfa648
DeepakM73 <deepak.muddasani73@gmail.com>
2021-08-12 02:36:41 +0530

    Emp Switch Case

--- src/com/employeewage/EmpWageComputation.java
+++ src/com/employeewage/EmpWageComputation.java
@@ -1,18 +1,39 @@
 package com.employeewage;
 
+/**
+ * EmpWage implements and application which will check employee status
+ *
+ * @author Deepak
+ * @version 1.0
+ * @since 11/08/2021
+ */
 public class EmpWageComputation {
 
     public static void main(String[] args) {
-        final int FULL_TIME = 1;
-        int empType = (int) (Math.random() * 100) % 2;
-        if (empType == FULL_TIME)
+        /**
+         * Method will check Employee presence, part time and calculates wage
+         * @param empType Generates Random variable from 0 t0 2
+         * Used Switch Case
+         */
+        final int FULL_TIME = 2;
+        final int PART_TIME = 1;
+        final int WAGE_PER_HR = 20;
+        int empType = (int) (Math.random() * 100) % 3;
+        int workingHours = 0;
+        switch (empType)
         {
-            System.out.println("Employee is Present");
-
-        } else
-        {
-            System.out.println("Employee is Absent");
-
+            case FULL_TIME:
+                System.out.println("Employee is Present Full time");
+                workingHours = 8;
+                break;
+            case PART_TIME:
+                System.out.println("Employee is Present Part time");
+                workingHours = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent");
         }
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
     }
 }
