b00349f2f2a0c687248fac94b0d2d6e3f0335f8d
DeepakM73 <deepak.muddasani73@gmail.com>
2021-08-12 01:17:58 +0530

    Employee Wage

--- src/com/employeewage/EmpWageComputation.java
+++ src/com/employeewage/EmpWageComputation.java
@@ -1,18 +1,33 @@
 package com.employeewage;
 
+/**
+ * EmpWage implements and application which will check employee status
+ *
+ * @author Deepak
+ * @version 1.0
+ * @since 11/08/2021
+ */
 public class EmpWageComputation {
 
     public static void main(String[] args) {
+        /**
+         * Method will check Employee presence and calculates wage
+         */
         final int FULL_TIME = 1;
+        final int WAGE_PER_HR = 20;
         int empType = (int) (Math.random() * 100) % 2;
+        int workingHours = 0;
         if (empType == FULL_TIME)
         {
             System.out.println("Employee is Present");
+            workingHours = 8;
 
         } else
         {
             System.out.println("Employee is Absent");
 
         }
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
     }
 }
