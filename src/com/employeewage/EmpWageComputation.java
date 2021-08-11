f56cc264e1919fc6767d019e20c83cc0489cecf1
DeepakM73 <deepak.muddasani73@gmail.com>
2021-08-12 01:11:27 +0530

    Employee Check

--- src/com/employeewage/EmpWageComputation.java
+++ src/com/employeewage/EmpWageComputation.java
@@ -1,8 +1,18 @@
 package com.employeewage;
 
+/**
+ * EmpWage implements and application which will check employee status
+ *
+ * @author Deepak
+ * @version 1.0
+ * @since 11/08/2021
+ */
 public class EmpWageComputation {
 
     public static void main(String[] args) {
+        /**
+         * Method will check Employee Presence
+         */
         final int FULL_TIME = 1;
         int empType = (int) (Math.random() * 100) % 2;
         if (empType == FULL_TIME)
